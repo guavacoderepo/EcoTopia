e2db9482ed0c924970e219d2bbcbdb69ad3b6d28
guavacoderepo <guavacodeplus@gmail.com>
2023-08-05 11:01:47 +0100

    new commit

--- controllers/usersController.py
+++ controllers/usersController.py
@@ -4,26 +4,26 @@ from datetime import datetime
 # init
 db = mongo
 
-
 def handleNewUser(name, email, phone, username, token, hash_pwd):
     # return new user date
-    return db.users.insert_one({"Name": name,
-                                "Email": email,
-                                "Phone": phone,
-                                "Username": username,
-                                "Password": hash_pwd,
-                                "Balance": 2.0,
-                                "Address": token,
-                                "Transactions": [],
-                                "Cart": [],
-                                "LastScan": datetime.now(),
-                                "created_at": datetime.now()
-                                })
-
+    return db.users.insert_one(
+        {   
+            "Name": name,
+            "Email": email,
+            "Phone": phone,
+            "Username": username,
+            "Password": hash_pwd,
+            "Balance": 2.0,
+            "Address": token,
+            "Transactions": [],
+            "Cart": [],
+            "LastScan": datetime.now(),
+            "created_at": datetime.now()
+        }
+    )
 
 def handleFetchUser(username):
     return db.users.find_one({"Username": username})
-
 
 # user find section
 def handleFindUser(username):
